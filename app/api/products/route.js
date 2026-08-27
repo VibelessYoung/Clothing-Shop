d6c6a199895829f6413e6ea54858f213fd67ac0a
Amir <vibelessyoung@gmail.com>
2026-08-27 17:12:28 +0330

    install mongoose

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,7 +1,12 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+
+  const gender = searchParams.get("gender");
+
   return NextResponse.json({
-    message: "Products API is working!",
+    message: "gender API",
+    Gender: gender,
   });
 }
