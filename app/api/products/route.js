a366eb524b9001546f32c1499537cf5affd3f136
Amir <vibelessyoung@gmail.com>
2026-08-28 15:27:52 +0330

    add filtring to api

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -32,11 +32,69 @@ export async function POST(request) {
     );
   }
 }
-export async function GET() {
+export async function GET(request) {
   try {
     await connectDB();
 
-    const products = await Product.find();
+    const { searchParams } = new URL(request.url);
+
+    const category = searchParams.get("category");
+    const gender = searchParams.get("gender");
+    const search = searchParams.get("search");
+
+    const minPrice = searchParams.get("minPrice");
+    const maxPrice = searchParams.get("maxPrice");
+
+    const sort = searchParams.get("sort");
+
+    const page = Number(searchParams.get("page")) || 1;
+    const limit = Number(searchParams.get("limit")) || 12;
+
+    const filter = {};
+
+    if (category) {
+      filter.category = category;
+    }
+
+    if (gender) {
+      filter.gender = gender;
+    }
+
+    if (search) {
+      filter.name = {
+        $regex: search,
+        $options: "i",
+      };
+    }
+
+    if (minPrice || maxPrice) {
+      filter.price = {};
+
+      if (minPrice) {
+        filter.price.$gte = Number(minPrice);
+      }
+
+      if (maxPrice) {
+        filter.price.$lte = Number(maxPrice);
+      }
+    }
+
+    let sortOption = {};
+
+    if (sort === "price-asc") {
+      sortOption = { price: 1 };
+    }
+
+    if (sort === "price-desc") {
+      sortOption = { price: -1 };
+    }
+
+    const skip = (page - 1) * limit;
+
+    const products = await Product.find(filter)
+      .sort(sortOption)
+      .skip(skip)
+      .limit(limit);
 
     return NextResponse.json(products);
   } catch (error) {
