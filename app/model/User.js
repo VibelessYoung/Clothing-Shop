3931c9cfd5b41057a29e3d096ce5a0449733bfb2
Amir <vibelessyoung@gmail.com>
2026-08-29 16:51:26 +0330

    user-model created

---
+++ app/model/User.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    lastName: {
+      type: String,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
