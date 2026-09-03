1e7bb09279953fe5de154c7ed512fffe085e1218
Amir <vibelessyoung@gmail.com>
2026-09-03 17:45:06 +0330

    add delete for cart

--- app/api/cart/[productId]/route.js
+++ app/api/cart/[productId]/route.js
@@ -89,3 +89,78 @@ export async function PATCH(request, { params }) {
     );
   }
 }
+export async function DELETE(request, { params }) {
+  try {
+    await connectDB();
+
+    const user = await getAuthenticatedUser(request);
+
+    if (!user) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const { productId } = await params;
+
+    const body = await request.json();
+
+    const { selectedSize, selectedColor } = body;
+
+    if (!selectedSize || !selectedColor) {
+      return NextResponse.json(
+        {
+          message: "selectedSize and selectedColor are required",
+        },
+        { status: 400 },
+      );
+    }
+
+    const cart = await Cart.findOne({
+      user: user._id,
+    });
+
+    if (!cart) {
+      return NextResponse.json(
+        {
+          message: "Cart not found",
+        },
+        { status: 404 },
+      );
+    }
+
+    const itemIndex = cart.items.findIndex(
+      (item) =>
+        item.product.toString() === productId &&
+        item.selectedSize === selectedSize &&
+        item.selectedColor === selectedColor,
+    );
+
+    if (itemIndex === -1) {
+      return NextResponse.json(
+        {
+          message: "Cart item not found",
+        },
+        { status: 404 },
+      );
+    }
+
+    cart.items.splice(itemIndex, 1);
+
+    await cart.save();
+
+    await cart.populate("items.product");
+
+    return NextResponse.json({
+      message: "Cart item removed successfully",
+      cart,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      {
+        message: "Failed to remove cart item",
+      },
+      { status: 500 },
+    );
+  }
+}
